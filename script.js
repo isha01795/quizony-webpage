3bbdf28784cc5d5ff09c211c4cc5b6564b9fd67d
Isha <isha01795@gmail.com>
2021-07-11 22:31:18 +0530

    quizony-webpage
    here is a responsive quizony webpage

---
+++ script.js
@@ -0,0 +1,42 @@
+
+     function toggle(){
+        var nav = document.getElementById("toggle-bar");
+        if( nav.style.display === "block"){
+            nav.style.display = "none";
+        }
+        else{
+            nav.style.display = "block";
+        }
+    }
+
+  
+    function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+function openLogForm() {
+  document.getElementById("myLogForm").style.display = "block";
+}
+
+function closeLogForm() {
+  document.getElementById("myLogForm").style.display = "none";
+}
+function openSignupForm() {
+  document.getElementById("mySignupForm").style.display = "block";
+}
+
+function closeSignupForm() {
+  document.getElementById("mySignupForm").style.display = "none";
+}
+function openLoginForm() {
+  document.getElementById("myLoginForm").style.display = "block";
+}
+
+function closeLoginForm() {
+  document.getElementById("myLoginForm").style.display = "none";
+}
+  
